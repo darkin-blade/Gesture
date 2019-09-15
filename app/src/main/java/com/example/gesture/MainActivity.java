3d5afb3c05d96556f66c070f54a60a169b741580
Lynx <1197225628@qq.com>
2019-09-15 08:38:27 +0800

    first

---
+++ app/src/main/java/com/example/gesture/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.gesture;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private GestureOverlayView gesture;
+
+    public String gesturePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        setGesture();
+    }
+
+    public void initData() {
+        // 检查权限
+        String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
+        int check_result = ActivityCompat.checkSelfPermission(this, permission);// `允许`返回0,`拒绝`返回-1
+        if (check_result != PackageManager.PERMISSION_GRANTED) {// 没有`写`权限
+            ActivityCompat.requestPermissions(this, new String[]{permission}, 1);// 获取`写`权限
+        }
+
+        gesturePath = getExternalFilesDir("").toString() + "/gesture";
+    }
+
+    public void setGesture() {
+        //获取手势编辑组件后，设置相关参数
+        gesture = (GestureOverlayView) findViewById(R.id.gesture);
+        gesture.setGestureColor(Color.GREEN);
+        gesture.setGestureStrokeWidth(5);
+        gesture.addOnGesturePerformedListener(new GestureOverlayView.OnGesturePerformedListener() {
+            @Override
+            public void onGesturePerformed(GestureOverlayView gestureOverlayView, final Gesture gesture) {
+                View saveDialog = getLayoutInflater().inflate(R.layout.dialog_save,null,false);
+                ImageView img_show = (ImageView) saveDialog.findViewById(R.id.img_show);
+                final EditText edit_name = (EditText) saveDialog.findViewById(R.id.edit_name);
+                Bitmap bitmap = gesture.toBitmap(128,128,10,0xffff0000);
+                img_show.setImageBitmap(bitmap);
+                new AlertDialog.Builder(MainActivity.this).setView(saveDialog)
+                        .setPositiveButton("保存",new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //获取文件对应的手势库
+                                GestureLibrary gestureLib = GestureLibraries.fromFile(gesturePath);// TODO 路径
+                                gestureLib.addGesture(edit_name.getText().toString(),gesture);
+                                gestureLib.save();
+                            }
+                        }).setNegativeButton("取消", null).show();
+            }
+        });
+    }
+}
